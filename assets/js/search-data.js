fb57042fbeba13b4b495ca116f72bca68fc5fa72
Kazi-Sher <36130623+Kazi-Sher@users.noreply.github.com>
2026-01-25 23:27:16 +0000

    Deploying to gh-pages from @ Kazi-Sher/kazi-sher.github.io@924cc9e8a98649f4cb707acc5c925c5338cb4f5c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,30 +492,4 @@ ninja.data = [{
         handler: () => {
           window.open("mailto:%6B%61%7A%69%73%68%65%72%61%68%6D%65%64@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
